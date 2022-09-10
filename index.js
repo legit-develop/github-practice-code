95f2e75ccc42dc5ba16c644fc542c2971cc7aa76
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2022-09-10 15:14:57 +0200

    add button functionality

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const btnEl = document.querySelector(".btn");
 
+btnEl.addEventListener("mouseover", (e) => {
+  const letter = btnEl.offsetLeft;
+  const let_2 = btnEl.offsetTop;
+  console.log(let_2);
+  console.log(letter);
+});
