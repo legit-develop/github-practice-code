13fc54a97b864102dd881f580f2783cf46c723ee
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2022-09-21 07:56:18 +0200

    reset file for another version

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const kits = ["crash", "bass", "tom", "snare"];
-const containerEl = document.querySelector(".container");
 
-kits.forEach((kit) => {
-  const btnEl = document.createElement("button");
-  btnEl.innerText = kit;
-  btnEl.classList.add("btn");
-  containerEl.appendChild(btnEl);
-});
